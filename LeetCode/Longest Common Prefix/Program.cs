0831149f535ea350600cb4c0406478c92eadfc55
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-12-20 14:42:04 +0700

    init project

---
+++ LeetCode/Longest Common Prefix/Program.cs
@@ -0,0 +1,23 @@
+﻿//Problem: Write a function to find the longest common prefix string amongst an array of strings.
+//Write a function to find the longest common prefix string amongst an array of strings.
+//If there is no common prefix, return an empty string "".
+    
+
+//Test case 1 : strs = ["flower","flow","flight"]
+string[] strs = new string[] { "flower", "flow", "flight" };
+Console.WriteLine(LongestCommonPrefix(strs));
+
+//Test case 2 : Input: strs = ["dog","racecar","car"]
+string LongestCommonPrefix(string[] strs) {
+    if (strs.Length == 0)
+        return "";
+    string prefix = strs[0];
+    for (int i = 1; i < strs.Length; i++)
+        while (strs[i].IndexOf(prefix) != 0) {
+            prefix = prefix.Substring(0, prefix.Length - 1);
+            if (prefix == "")
+                return "";
+        }
+
+    return prefix;
+}
---
+++ LeetCode/Roman to Integer/Program.cs
@@ -0,0 +1,41 @@
+﻿// URI: https://leetcode.com/problems/roman-to-integer/
+//Problem: Roman to Integer
+//Given a roman numeral, convert it to an integer.
+//Example 1:
+//Input: s = "III"
+//Output: 3
+var result = RomanToInt("MCMXCIV");
+Console.WriteLine(result);
+
+int RomanToInt(string s)
+{
+    Dictionary<char, int> romanValues = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+    int total = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        var currentValue = romanValues[s[i]];
+        if(i < s.Length - 1)
+        {
+            var nextValue = romanValues[s[i + 1]];
+            if(currentValue < nextValue)
+            {
+                total += nextValue - currentValue;
+                i++;
+                continue;
+            }
+        }
+        total += currentValue;
+    }
+    return total;
+
+}
